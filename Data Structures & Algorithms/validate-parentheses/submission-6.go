877cdc9bff053293979590c16892358d5af1e95f
FadhilR12 <82567187+FadhilR12@users.noreply.github.com>
2026-07-30 14:54:05 +0700

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.go
@@ -0,0 +1,37 @@
+type Stack struct {
+	par []string
+}
+
+func isValid(s string) bool {
+	var p Stack
+	
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '{' || s[i] == '[' {
+			p.par = append(p.par, string(s[i]))
+		}
+		if len(p.par) == 0 {
+    		return false
+		}
+		if s[i] == ')' || s[i] == '}' || s[i] == ']' {
+			top := p.pop()
+			if s[i] == ')' && top != "(" {
+				return false
+			}
+			if s[i] == '}' && top != "{" {
+				return false
+			}
+			if s[i] == ']' && top != "[" {
+				return false
+			}
+		}
+
+	}
+	return len(p.par) == 0
+}
+
+func (s *Stack) pop() string {
+	l := len(s.par) - 1
+	par := s.par[l]
+	s.par = s.par[:l]
+	return par
+}
